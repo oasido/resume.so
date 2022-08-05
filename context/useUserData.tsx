4373e5c416a1219824212693da212e91606f89b4
Ofek Asido <asido.ofek@gmail.com>
2022-08-06 02:18:44 +0300

    chore: set fields and setfields, remove comment

--- context/useUserData.tsx
+++ context/useUserData.tsx
@@ -1,16 +1,12 @@
-import { UserData } from 'types/user_data';
+import { UserData, Fields } from 'types/user_data';
 import create from 'zustand';
 
 export const useUserData = create<UserData>((set) => ({
-  name: '',
-  setName: (value) => set(() => ({ name: value })),
-
-  // sections: ['Heading', 'About', 'Work Experience', 'Projects', 'Education'],
-  // setSections: (value) => set(() => ({ sections: value })),
-  // selected: 0,
-  // setSelected: (idx) => set(() => ({ selected: idx })),
-  // addSectionModalOpened: false,
-  // setAddSectionModalOpened: (bool) => set(() => ({ addSectionModalOpened: bool })),
-  // newSection: '',
-  // setNewSection: (value) => set(() => ({ newSection: value })),
+  fields: {
+    name: '',
+    email: '',
+    linkedin: '',
+  },
+  setFields: (data: Partial<Fields>) =>
+    set(({ fields }: any) => ({ fields: { ...fields, ...data } })),
 }));
