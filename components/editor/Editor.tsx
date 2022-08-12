6c32651b28811ba2393582c8b02b83075b693c4b
Ofek Asido <asido.ofek@gmail.com>
2022-08-13 01:37:36 +0300

    chore: render markdown textarea instead of individual inputs

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -4,38 +4,16 @@ import { MarkdownTextarea } from '@components/editor/MarkdownTextarea';
 
 const useStyles = createStyles((theme) => ({
   container: {
-    padding: '0.5rem',
     backgroundColor: theme.colors.gray[1],
-
-    [theme.fn.largerThan('sm')]: {
-      padding: '1rem',
-    },
   },
 }));
 
 export const Editor = () => {
   const { classes } = useStyles();
-  const { sections, selected } = useStore();
 
-  const handleInputs = () => {
-    const currentSelectedSection = sections.length > 0 ? sections[selected].title : null;
-    switch (currentSelectedSection) {
-      case 'Heading':
-        return <MarkdownTextarea />;
-      case 'About':
-        break;
-      case 'Work Experience':
-        break;
-      case 'Projects':
-        break;
-      case 'Education':
-        break;
-      case null:
-        return 'Emptiness!';
-      default:
-        return 'some textarea';
-    }
-  };
-
-  return <div className={classes.container}>{handleInputs()}</div>;
+  return (
+    <div className={classes.container}>
+      <MarkdownTextarea />
+    </div>
+  );
 };
