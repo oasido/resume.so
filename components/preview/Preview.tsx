9b586bffa61d9110ccc2bff94b7de5b3186ba57f
Ofek Asido <asido.ofek@gmail.com>
2022-08-13 01:40:50 +0300

    feat: add copy button

--- components/preview/Preview.tsx
+++ components/preview/Preview.tsx
@@ -1,7 +1,9 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { createStyles } from '@mantine/core';
+import { ActionIcon, createStyles } from '@mantine/core';
 import { useStore } from '@context/useStore';
+import { AiFillCopy } from 'react-icons/ai';
+import { useClipboard } from '@mantine/hooks';
 
 const useStyles = createStyles((theme) => ({
   container: {
@@ -12,15 +14,26 @@ const useStyles = createStyles((theme) => ({
     minHeight: '301px',
   },
 
+  copy: {
+    padding: '0.5rem',
+    position: 'absolute',
+    right: 0,
+  },
 }));
 
 export const Preview = () => {
   const { classes } = useStyles();
   const { sections } = useStore();
   const markdown = sections.map((section) => section.text.replace('\n', '\n\n')).join('\r\n');
+  const clipboard = useClipboard();
 
   return (
     <div className={classes.container}>
+      <div className={classes.copy}>
+        <ActionIcon onClick={() => clipboard.copy(markdown)} variant="outline">
+          <AiFillCopy size={18} />
+        </ActionIcon>
+      </div>
       <ReactMarkdown remarkPlugins={[remarkGfm]}>{markdown}</ReactMarkdown>
     </div>
   );
