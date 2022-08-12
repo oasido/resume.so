62e8a4eac779c5ed4e3330c92bc4bcaa71948726
Ofek Asido <asido.ofek@gmail.com>
2022-08-13 02:01:03 +0300

    chore: add dummy data

--- context/useStore.tsx
+++ context/useStore.tsx
@@ -3,11 +3,17 @@ import create from 'zustand';
 
 export const useStore = create<Store>((set) => ({
   sections: [
-    { title: 'Heading', text: '' },
-    { title: 'About', text: '' },
-    { title: 'Work Experience', text: '' },
-    { title: 'Projects', text: '' },
-    { title: 'Education', text: '' },
+    {
+      title: 'Heading',
+      text: '# Ofek Asido\n### asido.ofek@gmail.com | github.com/oasido | ofekasido.xyz | (012)34567 | Israel\n',
+    },
+    { title: 'About', text: '## About\nLikes to pet dogs and eat food.\n' },
+    {
+      title: 'Work Experience',
+      text: '## Work Experience\n\n### Company 1 / 2015 - 2022\n_Role_\n* About the role',
+    },
+    { title: 'Education', text: '## Education\n### List Education here' },
+    { title: 'Projects', text: '## Projects\n### Project 1\n* Cool thing about the project' },
   ],
   setSections: (data: Section[]) => set(() => ({ sections: [...data] })),
   selected: 0,
