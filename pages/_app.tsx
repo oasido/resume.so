add1bb3410bafd7c0f921bc884f631e397243804
Ofek Asido <asido.ofek@gmail.com>
2022-08-13 12:28:13 +0300

    chore: change site title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>resume.so</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
 
